21d30d0b9b89f67e2f7031653a1e2372aed13268
Erin Stansbury <ekstansbury@gmail.com>
2020-01-01 22:49:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(number)
-  while number > 0
-      puts "#{number} SECOND(S)!\n"
-      number -= 1
+def countdown(num)
+  while num > 0
+      puts "#{num} SECOND(S)!\n"
+      num -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
