eb7956374711035b3f6510acb7652f65676f3c20
Erin Stansbury <ekstansbury@gmail.com>
2020-01-01 22:48:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,11 @@ def countdown(number)
 end
 
 def countdown_with_sleep(num)
-
-while num > 0
-
-  puts "#{num} SECOND(S)!"
-  sleep(1)
-  num -= 1
-
-end
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+    num -= 1
+  end
   "HAPPY NEW YEAR!"
-end	end
+end	
+
